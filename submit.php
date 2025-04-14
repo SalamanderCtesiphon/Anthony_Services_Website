17295c076d0c610b37aeb3657412bdfe16e2b1dc
Shannon Brookshire <swbrookshire@gmail.com>
2025-04-14 03:44:40 -0500

    trying to implement a contact form for the website using php

--- submit.php
+++ submit.php
@@ -1,3 +1,21 @@
 <?php
-var_dump($_POST);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   $name = htmlspecialchars($_POST['name']);
+   $email = htmlspecialchars($_POST['email']);
+   $message = htmlspecialchars($_POST['message']);
+
+   // Email configuration
+   $to = "swbrookshire@gmail.com";
+   $subject = "New Message from Contact Form";
+   $headers = "From: " . $email . "\r\n";
+   $headers .= "Reply-To: " . $email . "\r\n";
+   $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+
+   // Send email
+   if (mail($to, $subject, $message, $headers)) {
+      echo "Message sent successfully";
+   } else {
+      echo "Message could not be sent";
+   }
+}
 ?>
