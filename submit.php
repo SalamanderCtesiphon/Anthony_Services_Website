894bdf3574adeda3a5c4c16aa664217a02376746
Shannon Brookshire <swbrookshire@gmail.com>
2025-04-16 04:41:59 -0500

    trying to get the submit form php function to work

--- submit.php
+++ submit.php
@@ -4,7 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    $to = "your-email@example.com"; // Replace with your email address
+    $to = "swbrookshire@gmail.com"; // Replace with your email address
     $subject = "New Contact Form Submission";
     $body = "Name: $name\nEmail: $email\nMessage: $message";
 
